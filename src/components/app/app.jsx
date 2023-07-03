4ed639bfbd2921eba0abbd432cbf45053f5ba74e
Alexandr Slobodyanin <sashaslab@yandex.ru>
2023-07-03 15:20:00 +0300

    upd

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,7 +4,12 @@ import { data } from "../../utils/data";
 function App() {
   return (
     <div className={styles.app}>
-    
+      <pre style={{
+      	margin: "auto",
+      	fontSize: "1.5rem"
+      }}>
+      	Измените src/components/app/app.jsx и сохраните для обновления.
+      </pre>
     </div>
   );
 }
