3bb5f2feca4b29b74f27795cfd5b7f390857d265
Alexandr Slobodyanin <sashaslab@yandex.ru>
2023-07-08 20:54:17 +0300

    Revert "step2 completed"
    This reverts commit 6608ec335935bfc1e72490e1eae5610be0c3ae4d.

--- src/components/AppHeader/appHeader.jsx
+++ src/components/AppHeader/appHeader.jsx
@@ -1,30 +1,30 @@
-import { Logo, BurgerIcon, ProfileIcon, ListIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import {Logo, BurgerIcon, ProfileIcon, ListIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
-import style from './appHeader.module.css';
+import headerStyle from './appHeader.module.css';
 
 function AppHeader() {
     return (
-        <header className={style.header}>
-            <div className={style.header_desktop}>
-                <div className={style.logo}>
-                    <Logo />
-                </div>
-                <nav className='pl-5 pr-5 pt-4 pb-4'>
-                    <ul className={`${style.list} pt-4 pb-4`}>
-                        <li className={style.item}><a href="#constructor" className={style.link}>
-                            <BurgerIcon type="primary" />
-                            <p className="text text_type_main-default pl-2">Конструктор</p></a></li>
-                        <li className={`${style.item}`}><a href="#orders" className={style.link}>
-                            <ListIcon type="secondary" />
-                            <p className="text text_type_main-default text_color_inactive pl-2">Лента заказов</p></a></li>
-                        <li className={style.item}><a href="#account" className={style.link}>
-                            <ProfileIcon type="secondary" />
-                            <p className="text text_type_main-default text_color_inactive pl-2">Личный кабинет</p></a></li>
-                    </ul>
-                </nav>
+        <header className={headerStyle.header}>
+            <div className={headerStyle.header_desktop}>
+            <div className={headerStyle.logo}>
+            <Logo />
+            </div>
+            <nav className='pl-5 pr-5 pt-4 pb-4'>
+                <ul className={`${headerStyle.list} pt-4 pb-4`}>
+                   <li className={headerStyle.item}><a href="#constructor" className={headerStyle.link}>
+                    <BurgerIcon type="primary" />
+                    <p className="text text_type_main-default pl-2">Конструктор</p></a></li>
+                   <li className={`${headerStyle.item}`}><a href="#orders" className={headerStyle.link}>
+                    <ListIcon type="secondary" />
+                    <p className="text text_type_main-default text_color_inactive pl-2">Лента заказов</p></a></li>
+                   <li className={headerStyle.item}><a href="#account" className={headerStyle.link}>
+                    <ProfileIcon type="secondary" />
+                    <p className="text text_type_main-default text_color_inactive pl-2">Личный кабинет</p></a></li> 
+                </ul>
+            </nav>
             </div>
         </header>
-
+        
     )
 }
 
